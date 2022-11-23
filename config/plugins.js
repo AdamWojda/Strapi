a129754e37cabc7e88f1497245b9730ea7054a45
adam-hyd <adam.wojda@hyd.agency>
2022-11-23 16:43:45 +0100

    dsadasdas

--- config/plugins.js
+++ config/plugins.js
@@ -21,17 +21,4 @@ module.exports = ({ env }) => ({
         },
       },
     },
-    'transformer': {
-      enabled: true,
-      config: {
-        prefix: '/api/',
-        responseTransforms: {
-          removeAttributesKey: true,
-          removeDataKey: true,
-        }
-      }
-    },
-    seo: {
-      enabled: true,
-    },
   });
