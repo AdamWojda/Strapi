b9153a898dac2ffcf0e902d608217abbb0efb813
adam-hyd <adam.wojda@hyd.agency>
2022-11-23 17:14:16 +0100

    dsadasdas

--- config/database.js
+++ config/database.js
@@ -3,12 +3,15 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     debug: true,
     connection: {
-      host: env('PGHOST', 'containers-us-west-33.railway.app'),
-      port: env.int('PGPORT', 5980),
-      database: env('PGDATABASE', 'railway'),
-      user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'l2On9haFkxaJxndnCuPy'),
+      host: env('PGHOST', process.env.PGHOST),
+      port: env.int('PGPORT', process.env.PGPORT),
+      database: env('PGDATABASE', process.env.PGDATABASE),
+      user: env('PGUSER', process.env.PGUSER),
+      password: env('PGPASSWORD', process.env.PGPASSWORD),
       ssl: env.bool(true),
     },
+  },
+  settings: {
+    forceMigration: false
   },
 });
