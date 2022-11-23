6c9d9f6354b1fe6e55c5d79504d523d50cd252d7
adam-hyd <adam.wojda@hyd.agency>
2022-11-23 17:54:33 +0100

    dsadasdas

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,15 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
-      },
-    },
-    'import-export-entries': {
-      enabled: true,
-    },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  });
+  'transformer': {
+    enabled: true,
+    config: {
+      prefix: '/api/',
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      }
+    }
+  },
+  seo: {
+    enabled: true,
+  },
+});
